605d2d21ccae4a6403362c8aebeb3c5bd7e00610
Edgar Vergara <edgarvm13@hotmail.com>
2024-08-30 16:45:55 +0200

    ✨ api endpoints

--- src/app/api/tasks/route.js
+++ src/app/api/tasks/route.js
@@ -1,5 +1,18 @@
 import { NextResponse } from "next/server";
 
-export function GET() {
-    return NextResponse.json("creating tasks")
+import { prisma } from "@/libs/prisma";
+
+export async function GET() {
+    const tasks = await prisma.task.findMany();
+    return NextResponse.json(tasks);
+}
+
+export async function POST(request) {
+    const data = await request.json();
+
+    const newTask = await prisma.task.create({
+        data
+    });
+
+    return NextResponse.json(newTask);
 }
